51af11bcbb00e0a5d72e954843e3a53de9378df0
bhargavaE <bhargava272@gmail.com>
2021-05-29 21:40:30 +0530

    Update omdb1.py

--- omdb1.py
+++ omdb1.py
@@ -1,7 +1,7 @@
 import pprint
 import pprintpp
 from omdbapi.movie_search import GetMovie
-movie = GetMovie(title='Interstellar', api_key='e26e533a', plot='full')
+movie = GetMovie(title='The Revenant', api_key='e26e533a', plot='full')
 movie.get_all_data()
 pprint.pprint(movie.get_all_data())
 
